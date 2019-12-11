f10481644b72ffbfee11896234d829b70fd2c60a
Victor Vuelma <victorvuelma@outlook.com>
2019-12-11 14:57:29 -0300

    create dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdChevronRight, MdAddCircleOutline } from 'react-icons/md';
+
+import { Container, MeetupList, Meetup } from './styles';
 
 export default function Dashboard() {
-  return <div />;
+  return (
+    <Container>
+      <div>
+        <h1>Meus meetups</h1>
+        <button type="button">
+          <MdAddCircleOutline color="#fff" size={30} /> Novo Meetup
+        </button>
+      </div>
+      <MeetupList>
+        <Meetup>
+          <strong>Meetup de React Native</strong>
+          <div>
+            <span>24 de junho</span>
+            <button type="button">
+              <MdChevronRight color="#fff" size={30} />
+            </button>
+          </div>
+        </Meetup>
+        <Meetup>
+          <strong>Meetup de React Native</strong>
+          <div>
+            <span>24 de junho</span>
+            <button type="button">
+              <MdChevronRight color="#fff" size={30} />
+            </button>
+          </div>
+        </Meetup>
+      </MeetupList>
+    </Container>
+  );
 }
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+import { darken } from 'polished';
+
+export const Container = styled.div`
+  > div:first-child {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    h1 {
+      font-size: 32px;
+      font-weight: bold;
+      color: #fff;
+    }
+
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      width: 175px;
+      height: 45px;
+      color: #fff;
+      font-weight: bold;
+      font-size: 16px;
+      border: 0;
+      border-radius: 4px;
+      background: #f94d6a;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#f94d6a')};
+      }
+
+      svg {
+        margin-right: 10px;
+      }
+    }
+  }
+`;
+
+export const MeetupList = styled.div`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Meetup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  height: 65px;
+  background: rgba(0, 0, 0, 0.1);
+  padding: 0 20px 0 30px;
+  border-radius: 4px;
+
+  & + div {
+    margin-top: 10px;
+  }
+
+  strong {
+    font-size: 18px;
+    font-weight: bold;
+    color: #fff;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    span {
+      font-size: 16px;
+      color: rgba(255, 255, 255, 0.6);
+    }
+
+    button {
+      margin-left: 32px;
+      border: 0;
+      background: none;
+      transition: opacity 0.2s;
+
+      &:hover {
+        opacity: 0.7;
+      }
+    }
+  }
+`;
