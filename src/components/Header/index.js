e69c52f3076e5989e6e5b3062253fc0717bd65c9
Victor Vuelma <victorvuelma@outlook.com>
2019-12-11 00:50:50 -0300

    link header with profile and sign out action

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { Link } from 'react-router-dom';
+
+import { signOut } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
+  const dispatch = useDispatch();
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Content>
@@ -14,10 +25,12 @@ export default function Header() {
 
         <aside>
           <Profile>
-            <strong>Victor Vuelma</strong>
+            <strong>{profile.name}</strong>
             <Link to="/profile">Meu perfil</Link>
           </Profile>
-          <button type="button">Sair</button>
+          <button type="button" onClick={handleSignOut}>
+            Sair
+          </button>
         </aside>
       </Content>
     </Container>
